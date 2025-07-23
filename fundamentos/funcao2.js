6345f8b156c298b9f207050a95f532c6bb79c2b0
lecristina <cristinale33@hotmail.com>
2025-07-22 21:17:42 -0300

    funcao 1 e 2

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,17 @@
+// Armazenando uma função em uma variável
+const imprimirSoma = function(a, b) {
+    console.log(a + b);
+}
+
+imprimirSoma(2, 3); // 5
+
+// Armazenando uma função arrow em uma variável
+const soma = (a, b) => {
+    return a + b;
+}
+
+console.log(soma(2, 3)); // 5
+
+// retorno implícito em uma função arrow
+const subtracao = (a, b) => a - b;
+console.log(subtracao(2, 3)); // -1
